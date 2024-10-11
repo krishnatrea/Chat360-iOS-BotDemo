168a7f55cf6aba2d1b4135a745d9f81105448b78
Harshit Sharma <harshit.sharma@chat360.io>
2024-10-11 10:40:11 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Harshit Sharma on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
